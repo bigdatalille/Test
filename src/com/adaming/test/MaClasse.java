d16f8bf0d4169cf61bc542c799501a4ec17ed18e
BigDta Lille <bigdatalille@gmail.com>
2019-04-15 15:25:38 +0200

    first commit

---
+++ src/com/adaming/test/MaClasse.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adaming.test;
+
+/**
+ * @author inti
+ *
+ */
+public class MaClasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
